21fd453ace3c7d5d8f3c0d5d7964eaa97b83674a
jtwhitehd <jtwhitehd@gmail.com>
2022-09-17 17:59:17 -0500

    Add project files.

---
+++ YoutubeDownloaderWindowsGUI/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace YoutubeDownloaderWindowsGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(18, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(450, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Paste URL...";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(311, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 60);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.BulletIndent = 2;
+            this.richTextBox1.Location = new System.Drawing.Point(18, 63);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(273, 162);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "Title: \nResolution: \nAudio Bitrate: ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(338, 63);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 19);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "720p";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(395, 63);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(51, 19);
+            this.checkBox2.TabIndex = 9;
+            this.checkBox2.Text = "360p";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::YoutubeDownloaderWindowsGUI.Properties.Resources.YoutubeStill;
+            this.pictureBox1.Location = new System.Drawing.Point(311, 88);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::YoutubeDownloaderWindowsGUI.Properties.Resources.dimcord;
+            this.pictureBox2.Location = new System.Drawing.Point(395, 88);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(88, 71);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(484, 243);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(15);
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowIcon = false;
+            this.Text = "YoutubeDownloader";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ YoutubeDownloaderWindowsGUI/Form1.cs
@@ -0,0 +1,90 @@
+using VideoLibrary;
+/*  TODO !!
+ *      -> Modularize the program.  
+ *      The form should NOT be doing this processing!
+ *      FIX IT FOO
+ */
+
+namespace YoutubeDownloaderWindowsGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var url = textBox1.Text;
+            textBox1.Text = "Establishing connection to the provided URL..";
+            int resolution = 720;   // default to 720
+            int bitrate = 192;  // default
+            if (checkBox2.CheckState == CheckState.Checked)
+            {
+                resolution = 360;
+                bitrate = 96;
+            }
+            Task Download = Task.Run(() =>
+            {
+                try
+                {
+                    var youtube = YouTube.Default;
+                    var videos = youtube.GetAllVideos(url);
+                    int numberOfVideos = videos.Count();
+                    int i = 1;
+                    foreach (YouTubeVideo video in videos)
+                    {
+                        if (video.Resolution == resolution && video.AudioBitrate == bitrate)
+                        {
+                            var dir = Directory.GetCurrentDirectory().Substring(0, Directory.GetCurrentDirectory().Length - 14);
+                            textBox1.Text = "Connection established, attempting download...";
+                            richTextBox1.Text =
+                                $"Title: \"{video.Title}\"\nResolution: \"{video.Resolution}\"\nAudioBitrate: \"{video.AudioBitrate}\"\n";
+                            File.WriteAllBytes(dir + video.Title + ".mp4", video.GetBytes());
+                            textBox1.Text = "Download successful: " + Directory.GetCurrentDirectory();
+                            return;
+                        }
+                        else if (i == numberOfVideos)
+                        {
+                            textBox1.Text = "No suitable video file available at this address.";
+                        }    
+                        ++i;
+                    }
+                }
+                catch
+                {
+                    textBox1.Text = "Unable to complete request.  Test a different URL.";
+                }
+            });
+            await Download;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                checkBox2.CheckState = CheckState.Unchecked;
+            }
+            else
+            {
+                checkBox2.CheckState = CheckState.Checked;
+            }
+
+            //checkBox2.CheckState = CheckState.Unchecked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.CheckState == CheckState.Checked)
+            {
+                checkBox1.CheckState = CheckState.Unchecked;
+            }
+            else
+            {
+                checkBox1.CheckState |= CheckState.Checked;
+            }
+        }
+
+
+    }
+}
